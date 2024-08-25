5431c8b8ba016a29f476220bc3fc1770e6fdc403
Divya <divyawnd@gmail.com>
2024-08-25 17:49:21 +0530

    Admin panel user management

--- src/controllers/adminController.ts
+++ src/controllers/adminController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import jwt from "jsonwebtoken";
+import { UserModel } from "../models/userModel";
 
 class AdminController {
   // Static admin credentials
@@ -71,6 +72,50 @@ class AdminController {
       res.status(401).send("Invalid refresh token");
     }
   }
+
+  static async getDonors(req: Request, res: Response): Promise<void> {
+    try {
+      const donors = await UserModel.find({role: 'Donor'}); 
+      res.json(donors);
+    } catch (error) {
+      res.status(500).json({ message: 'Server error', error });
+    }
+  }
+
+  static async toggleActivation(req: Request, res: Response): Promise<void> {
+    try {
+      const { donorId, isActive } = req.body;
+
+      const updatedDonor = await UserModel.findByIdAndUpdate(
+        donorId,
+        { isActive: isActive },
+        { new: true }
+      );
+
+      if (!updatedDonor) {
+        res.status(404).json({ message: 'Donor not found' });
+        return;
+      }
+
+      res.status(200).json({
+        message: 'Donor activation status updated successfully',
+        donor: updatedDonor,
+      });
+    } catch (error) {
+      console.error('Error updating donor activation status:', error);
+      res.status(500).json({ message: 'Server error' });
+    }
+  }
+
+  static async getRecipients(req: Request, res: Response): Promise<void> {
+    try {
+      const recipients = await UserModel.find( {role: 'Recipient'} ); 
+      res.json(recipients);
+    } catch (error) {
+      res.status(500).json({ message: 'Server error', error });
+    }
+  }
+
 }
 
 export default AdminController;
--- src/routes/adminRoutes.ts
+++ src/routes/adminRoutes.ts
@@ -9,4 +9,10 @@ router.post("/login", AdminController.login);
 // Refresh token route
 router.post("/refresh-token", AdminController.refreshToken);
 
+router.get("/get_donors", AdminController.getDonors);
+
+router.post("/toggle_activation", AdminController.toggleActivation);
+
+router.get("/get_recipients", AdminController.getRecipients);
+
 export default router;
